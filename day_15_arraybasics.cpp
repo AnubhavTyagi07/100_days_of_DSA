aaaefca1f77966b3baa86ce4420f360ea8848c70
luckyanubhavtyagi-cmd <luckyanubhav.tyagi@gmail.com>
2026-06-29 14:05:16 +0530

    day_15:arraybasics

---
+++ day_15_arraybasics.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[100], n;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    cout << "Enter the array elements:" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "\nArray elements are:" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
