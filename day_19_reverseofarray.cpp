71b498e2eb47e08e7b7d0865b948b01293c6c104
luckyanubhavtyagi-cmd <260998402+AnubhavTyagi07@users.noreply.github.com>
2026-07-03 15:09:56 +0530

    day_19:reverseanarray

---
+++ day_19_reverseofarray.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[100];
+
+    cout << "Enter the array elements:" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int start = 0;
+    int end = n - 1;
+
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        start++;
+        end--;
+    }
+
+    cout << "Reversed array:" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
