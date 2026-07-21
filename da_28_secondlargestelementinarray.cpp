b8655a7be96384b028632c47edd2b11d03f63774
luckyanubhavtyagi-cmd <260998402+AnubhavTyagi07@users.noreply.github.com>
2026-07-21 23:44:24 +0530

    day 28:second largest element in array

---
+++ da_28_secondlargestelementinarray.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[100];
+
+    cout << "Enter the array elements:" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int largest = arr[0];
+    int secondLargest = -1;
+
+    for (int i = 1; i < n; i++) {
+
+        if (arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        }
+
+        else if (arr[i] > secondLargest && arr[i] != largest) {
+            secondLargest = arr[i];
+        }
+    }
+
+    if (secondLargest == -1)
+        cout << "No second largest element exists.";
+    else
+        cout << "Second Largest Element = " << secondLargest;
+
+    return 0;
+}
